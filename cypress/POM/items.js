883fb1cc07dc08db5686b1ee4108a243c4fd11b6
ttusk <74997041+ttusk@users.noreply.github.com>
2025-05-16 03:12:54 +0300

    Update automation tests for Advantage Shopping with BDD and page objects

--- cypress/POM/items.js
+++ cypress/POM/items.js
@@ -4,19 +4,31 @@ export default class items {
   }
 
   SelectItem() {
-    cy.get(".productName").first().click();
-  }
+  cy.intercept('/catalog/api/v1/products/*').as('getProduct');
+
+  cy.get(":nth-child(1) > :nth-child(4) > .productName")
+    .should('be.visible')
+    .click();
+
+  cy.wait('@getProduct');
+  cy.url().should('include', '/product/');
+}
+
+
 
   AddToCart() {
-    cy.get("#product .save").click();
+    cy.get('button[name="save_to_cart"]', { timeout: 15000 })
+      .should('be.visible')
+      .click({ force: true });
   }
 
   checkoutitem() {
     cy.get("#menuCart").click();
-    cy.get(".cart .checkOutButton").click();
+    // cy.get(".cart .checkOutButton").click();
+    cy.get("#checkOutButton").click();
   }
 
   cartPage() {
-    cy.url().should("include", "orderPayment");
+    cy.url().should('include', '/login'); 
   }
 }
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,27 +1,25 @@
-Cypress.Commands.add('usernameField', () => {
-    cy.get('[data-test="username"]')
-});
+// Cypress.Commands.add('usernameField', () => {
+//     cy.get('[data-test="username"]')
+// });
 
-Cypress.Commands.add('passwordField', () => {
-    cy.get('[data-test="password"]')
-});
+// Cypress.Commands.add('passwordField', () => {
+//     cy.get('[data-test="password"]')
+// });
 
-Cypress.Commands.add('loginBtn', () => {
-    cy.get('[data-test="login-button"]')
-});
+// Cypress.Commands.add('loginBtn', () => {
+//     cy.get('[data-test="login-button"]')
+// });
 
-Cypress.Commands.add('errorMsg', () => {
-    cy.get('[data-test="error"]')
-});
+// Cypress.Commands.add('errorMsg', () => {
+//     cy.get('[data-test="error"]')
+// });
 
-Cypress.Commands.add('loginWith', (username, password) => {
-    cy.visit('https://www.saucedemo.com')
-    cy.usernameField().type(username)
-    cy.passwordField().type(password)
-    cy.loginBtn().click()
-});
-
-
+// Cypress.Commands.add('loginWith', (username, password) => {
+//     cy.visit('https://www.saucedemo.com')
+//     cy.usernameField().type(username)
+//     cy.passwordField().type(password)
+//     cy.loginBtn().click()
+// });
 
 
 
@@ -29,14 +27,56 @@ Cypress.Commands.add('loginWith', (username, password) => {
 
 
 
+
+
+
+// Cypress.Commands.add("openStore", () => {
+//   cy.visit("https://advantageonlineshopping.com/#/");
+// });
+
+// Cypress.Commands.add("selectAndAddToCart", () => {
+//   cy.get("[aria-label='TabletsCategory']").click();
+
+//   cy.get(".productName").first().click();
+
+//   // Wait until the product page loads (URL contains '/product/')
+//   cy.url().should('include', '/product/');
+
+//   // Wait for the product API call to finish before interacting
+//   cy.intercept('/catalog/api/v1/products/*').as('getProduct');
+//   cy.wait('@getProduct');
+
+//   // Click the Add to Cart button with a longer timeout and force click if needed
+//   cy.get('button[name="save_to_cart"]', { timeout: 15000 })
+//     .should('be.visible')
+//     .click({ force: true });
+// });
+
+// commands.js
 
 Cypress.Commands.add("openStore", () => {
   cy.visit("https://advantageonlineshopping.com/#/");
 });
 
 Cypress.Commands.add("selectAndAddToCart", () => {
+  // Navigate to Tablets category
   cy.get("[aria-label='TabletsCategory']").click();
-  cy.get(".productName").first().click();
-  cy.get("#product .save").click();
-});
 
+  // Wait for product tiles to load and click the first full product card
+  cy.get(".categoryRight li")
+    .first()
+    .should("be.visible")
+    .click();
+
+  // Ensure we are on the product page
+  cy.url().should("include", "/product/");
+
+  // Intercept product API call to ensure the page is fully loaded
+  cy.intercept('/catalog/api/v1/products/*').as('getProduct');
+  cy.wait('@getProduct');
+
+  // Click the Add to Cart button reliably
+  cy.get('button[name="save_to_cart"]', { timeout: 15000 })
+    .should('be.visible')
+    .click({ force: true });
+});
